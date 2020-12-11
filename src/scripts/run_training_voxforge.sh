2a7993b67f456ea517fa26d07400bbb4587eb0b3
soerengustenhoff <soerengustenhoff@hotmail.com>
2020-12-11 22:07:44 +0100

    added files

--- src/scripts/run_training_voxforge.sh
+++ src/scripts/run_training_voxforge.sh
@@ -2,8 +2,8 @@ export TF_FORCE_GPU_ALLOW_GROWTH=true
 
 python -u DeepSpeech.py \
    --n_hidden 2048 \
-   --epochs 3 \
-   --checkpoint_dir ~/DeepSpeechCommander/checkpoints/ \
+   --epochs 1 \
+   --checkpoint_dir ~/DeepSpeechCommander/checkpoints \
    --train_files ~/datasets/voxforge/voxforge-train.csv \
    --dev_files ~/datasets/voxforge/voxforge-dev.csv \
    --test_files ~/datasets/voxforge/voxforge-test.csv \
@@ -11,6 +11,8 @@ python -u DeepSpeech.py \
    --dev_batch_size 24 \
    --test_batch_size 24 \
    --dropout_rate 0.05 \
+   --learning_rate 0.0001 \
+   --train_cudnn true
+
 #   --lm_alpha 0.75 \
 #   --lm_beta 1.85 \
-   --learning_rate 0.0001 
