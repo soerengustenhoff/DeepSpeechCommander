055af25eb6aba7dd759b9f156c78ff8e75ab8148
soerengustenhoff <soerengustenhoff@hotmail.com>
2020-12-16 01:22:26 +0100

    voice recognition using metadata
    added transcripton from metadata to filter through what deepspeech thought it might have heard

--- Python/main.py
+++ Python/main.py
@@ -9,6 +9,15 @@ import webrtcvad
 from halo import Halo
 from scipy import signal
 
+import json
+import sys
+import subprocess
+import shlex
+try:
+    from shhlex import quote
+except ImportError:
+    from pipes import quote
+
 logging.basicConfig(level=20)
 
 class Audio(object):
@@ -66,7 +75,7 @@ class Audio(object):
             data (binary): Input audio stream
             input_rate (int): Input audio rate to resample from
         """
-        data16 = np.fromstring(string=data, dtype=np.int16)
+        data16 = np.frombuffer(data, dtype=np.int16)
         resample_size = int(len(data16) / self.input_rate * self.RATE_PROCESS)
         resample = signal.resample(data16, resample_size)
         resample16 = np.array(resample, dtype=np.int16)
@@ -152,6 +161,84 @@ class VADAudio(Audio):
                     yield None
                     ring_buffer.clear()
 
+def convert_samplerate(audio_path, desired_sample_rate):
+    sox_cmd = 'sox {} --type raw --bits 16 --channels 1 --rate {} --encoding signed-integer --endian little --compression 0.0 --no-dither - '.format(quote(audio_path), desired_sample_rate)
+    try:
+        output = subprocess.check_output(shlex.split(sox_cmd), stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError('SoX returned non-zero status: {}'.format(e.stderr))
+    except OSError as e:
+        raise OSError(e.errno, 'SoX not found, use {}hz files or install it: {}'.format(desired_sample_rate, e.strerror))
+
+    return desired_sample_rate, np.frombuffer(output, np.int16)
+
+def words_from_candidate_transcript(metadata):
+    word = ""
+    word_list = []
+    word_start_time = 0
+    # Loop through each character
+    for i, token in enumerate(metadata.tokens):
+        # Append character to word if it's not a space
+        if token.text != " ":
+            if len(word) == 0:
+                # Log the start time of the new word
+                word_start_time = token.start_time
+
+            word = word + token.text
+        # Word boundary is either a space or the last character in the array
+        if token.text == " " or i == len(metadata.tokens) - 1:
+            word_duration = token.start_time - word_start_time
+
+            if word_duration < 0:
+                word_duration = 0
+
+            each_word = dict()
+            each_word["word"] = word
+            each_word["start_time"] = round(word_start_time, 4)
+            each_word["duration"] = round(word_duration, 4)
+
+            word_list.append(each_word)
+            # Reset
+            word = ""
+            word_start_time = 0
+
+    return word_list
+
+def metadata_json_output(metadata):
+    json_result = dict()
+    json_result["transcripts"] = [{
+        "confidence": transcript.confidence,
+        "words": words_from_candidate_transcript(transcript),
+    } for transcript in metadata.transcripts]
+    return json.dumps(json_result, indent=2)
+
+class TranscriptedWord:
+    confidence = -99.999
+    text = ""
+
+def handleVoiceOperation(transcriptedWords =  []):
+    voiceCommands = ["command", "start", "stop", "recieve", "transmit", "line", "one", "two", "three", "four"]
+
+    for transcriptedWord in transcriptedWords:
+        if transcriptedWord.text in voiceCommands:
+            print("Recognized: %s with confidence %.3f \n \n \n" % (transcriptedWord.text, transcriptedWord.confidence))
+            return transcriptedWord.text
+        return ""
+
+def handleMetadata(text):
+    transcriptedWords = []
+
+    transcripts = text.transcripts
+    for transcript in transcripts :
+        transcriptedWord = TranscriptedWord()
+        transcriptedWord.confidence = transcript.confidence
+        for token in transcript.tokens:
+            transcriptedWord.text += token.text
+        transcriptedWords.append(transcriptedWord)
+
+    text = handleVoiceOperation(transcriptedWords)
+    return text
+
 def main(ARGS):
     # Load DeepSpeech model
     if os.path.isdir(ARGS.model):
@@ -165,6 +252,25 @@ def main(ARGS):
     if ARGS.scorer:
         logging.info("ARGS.scorer: %s", ARGS.scorer)
         model.enableExternalScorer(ARGS.scorer)
+
+#    desired_sample_rate = model.sampleRate()
+#    audioPath = "sound//1.wav"
+#
+#    fin = wave.open(audioPath, 'rb')
+#    fs_orig = fin.getframerate()
+#    if fs_orig != desired_sample_rate:
+#        print(
+#            'Warning: original sample rate ({}) is different than {}hz. Resampling might produce erratic speech recognition.'.format(
+#                fs_orig, desired_sample_rate), file=sys.stderr)
+#        fs_new, audio = convert_samplerate(audioPath, desired_sample_rate)
+#    else:
+#        audio = np.frombuffer(fin.readframes(fin.getnframes()), np.int16)
+#
+#    audio_length = fin.getnframes() * (1 / fs_orig)
+#    fin.close()
+
+#    print(metadata_json_output(model.sttWithMetadata(audio, 3)))
+
 
     # Start audio with VAD
     vad_audio = VADAudio(aggressiveness=ARGS.vad_aggressiveness,
@@ -192,8 +298,7 @@ def main(ARGS):
             if ARGS.savewav:
                 vad_audio.write_wav(os.path.join(ARGS.savewav, datetime.now().strftime("savewav_%Y-%m-%d_%H-%M-%S_%f.wav")), wav_data)
                 wav_data = bytearray()
-            text = stream_context.finishStream()
-            print("Recognized: %s" % text)
+            text = handleMetadata(stream_context.finishStreamWithMetadata(50))
             if ARGS.keyboard:
                 from pyautogui import typewrite
                 typewrite(text)
